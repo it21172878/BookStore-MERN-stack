42a005dd7cc7b120e9f53f366c073cd4d6c56fb4
it21172878 <99156192+it21172878@users.noreply.github.com>
2023-09-22 10:21:09 +0530

    Update a book

--- backend/index.js
+++ backend/index.js
@@ -64,6 +64,30 @@ app.get('/books/:id', async (request, response) => {
   }
 });
 
+// Route for Update a book
+app.put('/books/:id', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found' });
+    }
+    return response.status(200).send({ message: 'Book Updated Successfully' });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose.connect(mongoDBURL).then(() => {
   try {
     console.log('App connected to database');
